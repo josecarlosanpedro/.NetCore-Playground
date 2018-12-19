ebe555cad18e23c1db8833ab574d86ac1abca7e7
Jose Carlo San Pedro <josecarlosanpedro0302@gmail.com>
2018-12-19 21:20:49 +0800

    change config

---
+++ CIty.API/Models/PointOfInterestDtoCreation.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CIty.API.Models
+{
+    public class PointOfInterestDtoCreation
+    {
+        public string Name {get; set;}
+        public string Description {get; set;}
+   }
+}
--- CIty.API/Startup.cs
+++ CIty.API/Startup.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.Extensions.Configuration;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace CIty.API
 {
@@ -19,9 +21,11 @@ namespace CIty.API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             // loggerFactory.AddConsole();
+            loggerFactory.AddConsole();
+            loggerFactory.AddDebug();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();

218d68b8b56af19267906fdb9b0fba33d0f0cecf
Jose Carlo San Pedro <josecarlosanpedro0302@gmail.com>
2018-12-19 21:26:53 +0800

    add appsetting.json

--- CIty.API/Startup.cs
+++ CIty.API/Startup.cs
@@ -13,6 +13,15 @@ namespace CIty.API
 {
     public class Startup
     {
+        public static IConfigurationRoot Configuration;
+
+        public Startup(IHostingEnvironment env) 
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddIniFile("appSettings.json", optional:false, reloadOnChange:true);
+            Configuration = builder.Build();
+        } 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
